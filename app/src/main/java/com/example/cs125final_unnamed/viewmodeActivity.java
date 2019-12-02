c6b8686ee3159f4eed12bc7df3a597a24576b0e0
leftiebagel <jacobwr3@illinois.edu>
2019-12-02 14:30:41 -0600

    buttonPushSetup 12/2 - 3

--- app/src/main/java/com/example/cs125final_unnamed/viewmodeActivity.java
+++ app/src/main/java/com/example/cs125final_unnamed/viewmodeActivity.java
@@ -3,6 +3,7 @@ package com.example.cs125final_unnamed;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class viewmodeActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class viewmodeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewmode);
+
+
+        Button backButton = findViewById(R.id.backButtonMap);
+        backButton.setOnClickListener(v -> {
+            finish();
+        });
     }
 }
