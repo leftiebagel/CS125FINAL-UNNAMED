5f355c79fe734fef5fac4f6f219f5b129610f5ab
leftiebagel <jacobwr3@illinois.edu>
2019-12-02 13:53:13 -0600

    buttonPushSetup 12/2 - 1

---
+++ app/src/main/java/com/example/cs125final_unnamed/loginActivity.java
@@ -0,0 +1,38 @@
+package com.example.cs125final_unnamed;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ */
+public class loginActivity extends AppCompatActivity {
+    /**
+     * Some older devices needs a small delay between UI widget updates
+     * and a change of the status and navigation bar.
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+
+        Button loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(v -> {
+            Intent newIntent = new Intent();
+            startActivity(newIntent);
+        });
+    }
+}
