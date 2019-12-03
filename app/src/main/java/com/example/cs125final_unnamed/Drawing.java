203ed785bbebf8c3b54aa2a8b4f87a0847f208d9
leftiebagel <jacobwr3@illinois.edu>
2019-12-03 16:00:12 -0600

    UICheckPoint - 12/3 - 3

---
+++ app/src/main/java/com/example/cs125final_unnamed/Drawing.java
@@ -0,0 +1,4 @@
+package com.example.cs125final_unnamed;
+
+public class Drawing {
+}
--- app/src/main/java/com/example/cs125final_unnamed/drawMap.java
+++ app/src/main/java/com/example/cs125final_unnamed/drawMap.java
@@ -1,9 +1,40 @@
 package com.example.cs125final_unnamed;
 
-public class drawMap {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
 
+import java.util.ArrayList;
+
+public class drawMap {
+    GoogleMap map;
+    LatLng mapCenter;
+
+    public drawMap (GoogleMap theMap, LatLng center) {
+        map = theMap;
+        mapCenter = center;
+        setupMap();
+    }
     //center map
+
+    private void setupMap() {
+        final float defaultMapZoom = 18f;
+        //LatLng location = new LatLng(40.013,-88.002); near champaign for testing purposes
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                mapCenter, defaultMapZoom));
+    }
+
     //start location function
+    public void draw(Drawing toDraw) {
+        //iterate through list of Lines, Draws them to map
+    }
+
+    public void draw(ArrayList<Drawing> toDraw) {
+        for (Drawing drawThis : toDraw) {
+            draw(drawThis);
+        }
+    }
+
     //adding lines
     //end location function
 }
